17f4fa617a54fc8226287aee1bc4f5d18c384adb
Kurnia Muhamad <omenkzz@gmail.com>
2022-01-28 13:57:26 +0700

    Add user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   layout "posts"
+  before_action :authenticate_user!
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  
   # GET /posts or /posts.json
   def index
     @posts = Post.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
   resources :posts
   root "home#index"
 end
