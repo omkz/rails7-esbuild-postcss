6b2be6b8024ec48db447a0d2c04dab917934ea9f
Kurnia Muhamad <omenkzz@gmail.com>
2022-01-28 14:56:19 +0700

    Update email template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,10 @@
 class UserMailer < ApplicationMailer
-    def welcome_email
-        @user = params[:user]
-        @url  = 'http://example.com/login'
-        mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-      end
+  layout "email"
+    
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
 end
